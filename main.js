22f7fff67054b07dc283007fa99b712420ca143d
Danielle Madeley <danielle.madeley@collabora.co.uk>
2012-03-30 14:54:46 +1100

    Hack in a theme from Dawati

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 const MainLoop = imports.mainloop;
 const Clutter = imports.gi.Clutter;
 
+imports.gi.versions.Mx = '2.0';
+const Mx = imports.gi.Mx;
+
 const Weather = imports.Weather;
 const Widget = imports.Widget;
 
 function main()
 {
   Clutter.init(null, null);
+
+  /* FIXME: hacky */
+  const PREFIX = '/home/danni/src/install';
+  Mx.Style.get_default().load_from_file(
+      PREFIX + '/share/dawati-shell/theme/datetime/date-panel.css');
 
   let stage = Clutter.Stage.get_default();
   let proxy = new Weather.AccuweatherProxy();
